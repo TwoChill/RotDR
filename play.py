37e0552a9369a5914f36e743255b0a4a18515bfb
Oren Eno <Oren@localhost.localdomain>
2019-11-08 01:39:53 +0100

    Added DEBUGGING comment code

--- play.py
+++ play.py
@@ -12,34 +12,34 @@ txtSpeed = 0.05
 try:
     func.sys_clear()
 
-    # location = 'Beginning'
+    # # DEBUGGING block
+    # usrName = 'Mike'
+    # usrGendr_Boy = ("he", "his", "him", "his",
+    #                 "He", "His", "Him", "His")
+    # usrGendr = usrGendr_Boy
 
-    # # Gets and prints the game logo.
-    # Game_Name = func.game_name()
-    # clss.Typing(0.001, Game_Name)
-    # time.sleep(2)
-    # func.sys_clear()
+    location = 'Beginning'
 
-    # # Gets the start menu.
-    # Start_Game = clss.Menus(location)
-    # Start_Game.start_menu(location)
+    # Gets and prints the game logo.
+    Game_Name = func.game_name()
+    clss.Typing(0.0005, Game_Name)
+    time.sleep(2)
+    func.sys_clear()
 
-    # # Create an instance of Hero with user input.
-    # usrName = ''
-    # usrGendr = ()
+    # Gets the start menu.
+    Start_Game = clss.Menus(location)
+    Start_Game.start_menu(location)
 
-    # # Create instance of Hero with a NAME and GENDER in var PLAYER
-    # player = clss.Hero(usrName, usrGendr, location)
-    # player.character_creation()
+    # Create an instance of Hero with user input.
+    usrName = ''
+    usrGendr = ()
 
-    # usrGendr = player.usrGendr
-    # usrName = player.usrName
+    # Create instance of Hero with a NAME and GENDER in var PLAYER
+    player = clss.Hero(usrName, usrGendr, location)
+    player.character_creation()
 
-    # DEBUG
-    usrName = 'Mike'
-    usrGendr_Boy = ("he", "his", "him", "his",
-                    "He", "His", "Him", "His")
-    usrGendr = usrGendr_Boy
+    usrGendr = player.usrGendr
+    usrName = player.usrName
 
     # Calls locator method to track player's location.
     location = 'Home'
